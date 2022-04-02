cda067018f40f9e9d1a5b0e622382458137850ee
ariful4082 <ariful4082@gmail.com>
2022-04-03 04:14:39 +0600

    validation setup

--- README.md
+++ README.md
@@ -17,19 +17,6 @@
 - > json schema
 - > json type defination
 
-### Express Validation Process
+### Understand Schema Validation
 
-- [ ] Introducing express-validator
-- [ ] Setup express validator
-- [ ] Get validation result
-- [ ] Name & Email Validation
-- [ ] Custom Validator for email
-- [ ] Password Validation
-- [ ] Optional Properties
-- [ ] Fixed Addresses and skills issues
-- [ ] Wildcard validation
-- [ ] Custom sanitizer for skills
-- [ ] Schema validation
-- [ ] Format validation result
-- [ ] validation result custom middleware
-- [ ] Wrap up
+- [ ] Just Theory
--- index.js
+++ index.js
@@ -1,102 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const { body, validationResult } = require("express-validator");
+const Joi = require('joi')
 
 const app = express();
 app.use([morgan("dev"), cors(), express.json()]);
 
-const reqBodyValidator = [
-  body("name")
-    .trim()
-    .isString()
-    .withMessage("Name must be a valid string")
-    .bail()
-    .isLength({ min: 5, max: 30 })
-    .withMessage("name length must be between 5-30 chars"),
-  body("email")
-    .normalizeEmail({ all_lowercase: true })
-    .isEmail()
-    .withMessage("Please provide a valid email")
-    .bail()
-    .custom((value) => {
-      if (value === "ariful@gmail.com") {
-        throw new Error("email already used");
-      }
-      return true;
-    }),
-  body("password")
-    .isString()
-    .withMessage("Password must be a valid string")
-    .bail()
-    .isLength({ min: 8, max: 30 })
-    .withMessage("Password length must be between 8-30 chars")
-    .bail()
-    .matches(/^[a-zA-Z0-9!@#$%^&*]{8,30}$/)
-    .withMessage(
-      "Password must contain uppercase, lowercase, digit ans special chars"
-    ),
-  body("confirmPassword")
-    .isString()
-    .withMessage("Confirm Password must be a valid string")
-    .bail()
-    .custom((value, { req }) => {
-      if (value !== req.body.password) {
-        throw new Error("Password Does not Match");
-      }
-      return true;
-    }),
-  body("bio")
-    .optional()
-    .trim()
-    .escape()
-    .isString()
-    .isLength({ min: 20, max: 300 })
-    .withMessage("bio must be between 20-300 chars"),
-  body("address")
-    .optional()
-    .custom((value) => {
-      if (!Array.isArray(value)) {
-        throw new Error("Address must be an array of address");
-      }
-      return true;
-    }),
-  body("addresses.*.postcode")
-    .isNumeric()
-    .withMessage("Postcode must be numeric")
-    .toInt(),
-  body("skills")
-    .optional()
-    .trim()
-    .not()
-    .isEmpty()
-    .withMessage("Skills must be a comma separated string")
-    .customSanitizer((value)=>{
-     return value.split(',').map(item => item.trim())
-    })
-    
-];
 
-// validate middleware
-const validate = (req, res, next)=>{
-  const errorFormatter = ({  msg, param }) => {
-    // Build your resulting errors however you want! String, object, whatever - it works!
-    return ` ${msg}`;
-  };
+// Post Schema
+const schema = Joi.object({
+  name: Joi.string().required()
+})
 
-  const errors = validationResult(req).formatWith(errorFormatter);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.mapped());
-  }
-  next()
-
-}
 
 // handle registration
-app.post("/", reqBodyValidator, validate, (req, res) => {
-
-  
-  console.log('Request Body', req.body);
+app.post("/",  (req, res) => {
+  const result = schema.validate(req.body);
+  if(result.error){
+    console.log(result.error.details);
+   return res.status(400).json(result.error.details)
+  }
+  console.log(result.value);
   res.status(201).json({ message: "Ok" });
 });
 
