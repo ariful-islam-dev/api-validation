8127331ab97564277a00bb23248458fe3b88655f
ariful4082 <ariful4082@gmail.com>
2022-04-02 08:20:30 +0600

    express validator setup

--- index.js
+++ index.js
@@ -1,77 +1,45 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const validator = require('validator')
+const { body } = require("express-validator");
 
 const app = express();
 app.use([morgan("dev"), cors(), express.json()]);
 
-const people = {
-  ariful: {
-    firstName: "Ariful",
-    lastName: "Islam",
-    email: "ariful@gmail.com",
-  },
-  aysha: {
-    firstName: "Aysha",
-    lastName: "Mony",
-    email: "aysha@gmail.com",
-  },
-};
+const reqBodyValidator = [
+  body("name"),
+  body("email"),
+  body("password"),
+  body("confirmPassword"),
+  body("bio"),
+  body("address"),
+  body("skills"),
+];
 
-app.get("/", (req, res) => {
-  const username = req.query.username;
-  if (!username) {
-    return res
-      .status(400)
-      .json({
-        error: "Bad Request",
-        message: "username query parameter missing",
-      });
-  }
-  res.status(200).json(people[username.toLocaleLowerCase()]);
-});
-
-app.post("/", (req, res) => {
-  const { name, username, email } = req.body;
-
-  if(!name){
-      return res.status(400).json({
-          error: 'Bad Request',
-          massage: 'name is missing'
-      })
-  }
-
-
-  if(!username){
-      return res.status(400).json({
-          error: 'Bad Request',
-          massage: 'username is missing'
-      })
-  }
-
-
-  if(!email){
-      return res.status(400).json({
-          error: 'Bad Request',
-          massage: 'email is missing'
-      })
-  }
-
-  if(!validator.isEmail(email)){
-      return res.status(400).json({
-          error: 'Bad Request',
-          message: 'Invalid Email'
-      })
-  }
-
-  const names = name.split(" ");
-  const firstName = names[0];
-  const lastName = names[1] || "";
-  people[username.toString()] = { firstName, lastName, email };
-  res.status(201).json(people[username.toLowerCase()]);
+// handle registration
+app.post("/", reqBodyValidator, (req, res) => {
+  res.status(201).json({ message: "Ok" });
 });
 
 app.listen(4000, () => {
   console.log("server is listening on port 4000");
 });
+
+// const body = {
+//   name: "Ariful Islam",
+//   email: "ariful@gmail.com",
+//   password: "1234",
+//   confirmPassword: "1234",
+//   bio: "Full-Stack developer",
+//   address: [
+//     {
+//       city: "Borisal",
+//       postcode: 8224,
+//     },
+//     {
+//       city: "Munshigonj",
+//       postcode: 1522,
+//     },
+//   ],
+//   skill: "Javascript, React, Node",
+// };
